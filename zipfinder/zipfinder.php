47e6ebcf851ad09b6afd2f6274e1cf1630553ced
Jose Contreras <jocontre@joselcontreras.com>
2015-01-28 22:48:35 -0600

    Added white div and got Maps API to work.

--- zipfinder/zipfinder.php
+++ zipfinder/zipfinder.php
@@ -15,21 +15,21 @@
 <body>
   <div class="container">
   	<div class="row">
-  	  <div class="col-md-6 col-md-offset-3 center">
+  	  <div class="col-md-6 col-md-offset-3 center whiteBackground">
     		<h1 class="center black">Zipcode Finder</h1>
 
-    		<p class="lead center black">Enter a address to find a zipcode.</p>
+    		<p class="lead center black">Enter an address to find the zipcode.</p>
 
     		<form>
     		  <div class="form-group">
-    		    <input type="text" class="form-control" name="city" id="city" placeholder="Eg. Lub-bick, Walla Walla, Lizard Lick..." />
+    		    <input type="text" class="form-control" name="address" id="address" placeholder="Eg. 321 Niam St. Nwotyna, AA" />
           </div>
-    		  <button id="findMyWeather" class="btn btn-success btn-lg">Get My Weather</button>
+    		  <button id="findMyZip" class="btn btn-success btn-lg">Find the ZIP!</button>
     		</form>
 
     		<div id="success" class="alert alert-success">Success</div>
-    		<div id="fail" class="alert alert-danger">Could not find the zip for that city. Try again.</div>
-    		<div id="noCity" class="alert alert-danger">Enter a city!</div>
+    		<div id="fail" class="alert alert-danger">Could not find the zip for that address. Try again.</div>
+    		<div id="noCity" class="alert alert-danger">You aren't connected to the internet. Maybe your neighbor turned off the modem.</div>
   	  </div>
   	</div>
   </div>
@@ -37,26 +37,40 @@
   <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
   <script>
 
-    $("#findMyWeather").click(function(event) {
-      event.preventDefault(); // This prevents form from submitting
-
+    $("#findMyZip").click(function(event) {
+      
+      var result = 0;
       $(".alert").hide();
 
-      if ($("#city").val()!="") {
-        $.get("scraper.php?city="+$("#city").val(), function(data) {
+      event.preventDefault();
 
-          if (data=="") {
-            $("#fail").fadeIn();
-          } 
+      $.ajax({
+        type: "GET".
+        url: "https://maps.googleapis.com/maps/api/geocode/xml?address="+encodeURIComponent($('#address').val())+"&sensor=false&key=AIzaSyBuY7mIUm2VjHoYvckJi97g7iZ3Cdfu4QM",
+        dataType: "xml",
+        success: processXML
+        error: error
+      });
 
-          else {
-            $("#success").html(data).fadeIn();
+      function processXML(xml){
+        $(xml).find("address_component").each(function(){
+          if($(this).find("type").text() == "postal_code"){
+            $("#success").html("The zipcode is"+$(this).find("long_name").text()).fadeIn();
+
+            result = 1;
           }
+
         });
-      } 
-      else {
-        $("#noCity").fadeIn();
-      }
+
+        if (result==0){
+          $("fail").fadeIn();
+        }
+      };
+
+      function error(){
+        $("noCity").fadeIn();
+      };
+
     });
 
   </script>
